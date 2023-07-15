be46d28a46aa8ec5c6160ec16772291b8f6a47a0
LeoRosero <roseroleo@yahoo.es>
2023-07-14 21:43:47 -0500

    ejercicios hasta 21

--- script.js
+++ script.js
@@ -65,6 +65,7 @@ switch (operacion) {
         break;
 }
 */
+
 /*
 6. Realizar un programa que, dado un número entero, visualice en pantalla si es par o impar.
 En caso de que el valor ingresado sea 0, se debe mostrar “el número no es par ni impar”.
@@ -131,45 +132,254 @@ console.log(nueva)
 
 /*
 10. Escribir una función flecha que reciba una palabra y la devuelva al revés.  
-*/
-
+function invertir(frase) {
+    let esarf = "";
+    for (let i = frase.length - 1; i >= 0; i--) {
+        esarf += frase[i];
+    }
+    return esarf;
+}
 var frase = prompt("Escriba una palabra");
-long = frase.length;
-frase.forEach(letra => {
-    
-});
-let func = (palabra) => a+blur;
+console.log(invertir(frase))
+
+//Otra version
+let frase = prompt("ingrese una frase ").split('');
+let a = (frase) => frase.reverse().join("");
+console.log(a(frase));
+*/
 
 /*
 11. Escribir una función que reciba un String y devuelva la palabra más larga.  
 String Ejemplo: “Guia de JavaScript”
 Resultado esperado : “JavaScript” 
+function masLarga(frase) {
+    let larga = "";
+    const palabras = frase.split(" ");
+    for (let i = 0; i < palabras.length-1; i++) {
+        if (palabras[i].length > larga.length) {
+            larga = palabras[i];
+        }
+    }
+    return larga;
+}
+var frase = prompt("Escriba una frase");
+console.log(masLarga(frase));
+*/
+
+/*
 12. Escribir una función flecha de JavaScript que reciba un argumento y retorne el tipo de
 dato.  
+var dato = prompt("Escriba un dato");
+let tipo = (dato) => console.log(typeof dato);
+tipo(dato);
+*/
+
+/*
 13. Crear un objeto persona, con las propiedades nombre, edad, sexo ('H' hombre, 'M' mujer,
 'O' otro), peso y altura. A continuación, muestre las propiedades del objeto JavaScript.  
+var persona = {
+    nombre : "lero",
+    edad : 51,
+    sexo : "H",
+    peso : 88,
+    altura : 1.71,
+}
+console.log(persona);
+*/
+
+/*
 14. Crear un objeto libro que contenga las siguientes propiedades: ISBN, Título, Autor,
 Número de páginas. Crear un método para cargar un libro pidiendo los datos al usuario 
 y luego informar mediante otro método el número de ISBN, el título, el autor del libro y el
-numero de páginas.  
+numero de páginas. 
+var libro = {
+    isbn : null, 
+    titulo : "", 
+    autor : "",
+    numpag : null,
+}
+
+function libroNuevo() {
+    libro.titulo = prompt("Titulo ")
+    libro.autor = prompt("Autor ");
+    libro.isbn = prompt("ISBN ");
+    libro.numpag = prompt("Numero de paginas ");
+}
+
+function mostrarLibro(libro) {
+    console.log(libro);
+}
+libroNuevo();
+mostrarLibro(libro);
+*/
+
+/*
 15. Escribe un programa JavaScript para calcular el área y el perímetro de un objeto Círculo
 con la propiedad radio. Nota: Cree dos métodos para calcular el área y el perímetro. El
-radio del círculo lo proporcionará el usuario.  
+radio del círculo lo proporcionará el usuario. 
+function area(radio) {
+    area = Math.PI*Math.pow(radio, 2);
+    return area;
+}
+function perimetro(radio) {
+    perimetro = Math.PI*2*radio;
+    return perimetro;
+}
+var radio = parseInt(prompt("Radio "));
+console.log(area(radio));
+console.log(perimetro(radio));
+*/
+
+/*
 16. Realizar un programa que rellene dos vectores al mismo tiempo, con 5 valores aleatorios
-y los muestre por pantalla.  
+y los muestre por pantalla. 
+const v1 = [];
+const v2 = [];
+for (let i = 0; i < 5; i++) {
+    v1[i] = parseInt(Math.random()*10);
+    v2[i] = parseInt(Math.random()*10);
+}
+console.log("vector 1 = "+JSON.stringify(v1));
+console.log("Vector 2 = "+JSON.stringify(v2));
+*/
+
+/*
 17. Realizar un programa que elimine los dos últimos elementos de un array. Mostrar el
 resultado 
+const arr = [1,2,3,4,5];
+console.log(arr);
+arr.pop();
+arr.pop();
+console.log(arr);
+*/
+
+/*
 18. A partir del siguiente array: var valores = [true, 5, false, "hola", "adios", 2]: 
 a) Determinar cual de los dos elementos de texto es mayor 
 b) Utilizando exclusivamente los dos valores booleanos del array, determinar los
 operadores necesarios para obtener un resultado true y otro resultado false 
 c) Determinar el resultado de las cinco operaciones matemáticas realizadas con los
 dos elementos numéricos 
+var valores = [true, 5, false, "hola", "adios", 2];
+var elementoMayor = "";
+var nuevo = [];
+valores.forEach(elemento => {
+    if (typeof elemento === "string") {
+        if (elemento.length > elementoMayor.length) {
+            elementoMayor = elemento;
+        }
+    }
+});
+console.log("elemento mayor = " + elementoMayor);
+for (let e of valores) {
+    if (e === true || e === false) {
+        nuevo.push(e);
+    }
+}
+console.log(JSON.stringify(nuevo));
+let s = 0, r = 0, m = 1, d = 0;
+for (let e of valores) {
+    if (typeof e === "number") {
+        console.log(e)
+        s += e;
+        r -= e;
+        m *= e;
+        if (d === 0){
+            d = e;
+            e = 1;
+        }
+        d /= e;
+    }
+}
+console.log("s="+s+" r="+r+" m="+m+" d="+d);
+*/
+
+/*
 19. Realizar un programa en Java donde se creen dos arreglos: el primero será un arreglo A
 de 50 números reales, y el segundo B, un arreglo de 20 números, también reales. El
 programa deberá inicializar el arreglo A con números aleatorios y mostrarlo por pantalla.
 Luego, el arreglo A se debe ordenar de menor a mayor y copiar los primeros 10 números
 ordenados al arreglo B de 20 elementos, y rellenar los 10 últimos elementos con el valor
 0.5. Mostrar los dos arreglos resultantes: el ordenado de 50 elementos y el combinado
-de 20. 
+de 20.
+const vA = [];
+const vB = [];
+function compararDosNumeros(a, b){
+    if (a > b) return 1;
+    if (a == b) return 0;
+    if (a < b) return -1;
+}
+
+for (let i = 0; i < 50; i++) {
+    vA[i] = parseInt(Math.random()*10);
+    
+}
+console.log(JSON.stringify(vA));
+vA.sort(compararDosNumeros);
+
+for (let i = 0; i < 20; i++) {
+    if (i<10) {
+        vB[i] = vA[i];
+    } else {
+        vB[i] = 0.5       
+    }
+    
+}
+console.log(JSON.stringify(vA));
+console.log(JSON.stringify(vB));
+*/
+
+/*
+20. Realizar un programa que obtenga la siguiente matriz [[3], [6], [9], [12], [15]] y devuelve y
+muestre el siguiente array [6, 9, 12, 15, 18].  
+const vA = [];
+const vB = [];
+for (let i = 0; i < 5; i++) {
+    vA[i] = (i+1) * 3;
+    vB[i] = (i+1) * 3 + 3;
+}
+console.log(JSON.stringify(vA));
+console.log(JSON.stringify(vB));
+*/
+
+/*
+21. Escribir un programa para obtener un array de las propiedades de un objeto Persona.
+Las propiedades son nombre, edad, sexo ('H' hombre, 'M' mujer, 'O' otro), peso y altura. 
+*/
+const Persona = {
+    nombre : "", 
+    edad : null, 
+    sexo : "",
+    peso : null,
+    altura : null,
+}
+const matriz = JSON.stringify(Persona);
+console.log(matriz);
+console.log(typeof matriz);
+/*
+22. Escribir un programa de JavaScript que al clickear un botón muestre un mensaje a
+elección.
+*/
+
+
+/*
+23. Resalte todas las palabras de más de 8 caracteres en el texto del párrafo (con un fondo
+amarillo, por ejemplo)  
+ 
+24. Escribir un programa de JavaScript que a través de un formulario calcule el radio de un 
+circulo y lo muestre en el HTML. 
+25. Escriba una función de JavaScript para obtener los valores de Nombre y Apellido del
+siguiente formulario.  
+<!DOCTYPE html>
+<html><head>
+<meta charset=utf-8 />
+<title>Obtener nombre y apellido de form </title>
+</head><body>
+<form id="form1" onsubmit="getFormValores()">
+Nombre: <input type="text" name="nombre" value="David"><br>
+Apellido: <input type="text" name="apellido" value="Beckham"><br>
+<input type="submit" value="Submit"> 
+</form>
+</body>
+</html> 
 */
